94d3f0b7860f7ec860c45c5d462da5f993c8638f
RohitE25 <kumarrohit.2512003@gmail.com>
2025-07-09 21:55:21 +0530

    login

--- src/components/Login/Backgroung.jsx
+++ src/components/Login/Backgroung.jsx
@@ -4,7 +4,9 @@ import React from "react";
 const Backgroung = () => {
   return (
     <div className="">
-      <img src="/loginbg.jpg" alt="aot" />
+      <div className="h-[860px] w-full absolute z-2 bg-black opacity-50">rohit</div>
+      <img className="z-0" src="/loginbg.jpg" alt="aot" />
+
 
     </div>
   );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Backgroung from "./Backgroung";
+import { Link } from "react-router";
+
 const Login = () => {
+  const [login, setLogin] = useState(false);
   return (
     <div className="">
-      <div className="w-full flex absolute top-0 z-50 h-20 items-center justify-between">
-        <div className="logo pl-4 pr-4 w-60 text-4xl text-white">
-          <img className="w-[65px]" src="/logo.png" />
-          <p className="text-4xl text-white flex"> Nim <p className="text-black">ory</p></p>
-        </div>
+      <div className="w-full fixed flex top-0 z-50 h-20 items-center justify-between">
+        <Link to="/">
+          <div className="logo pl-4 pr-4 w-60 text-4xl text-white">
+            <img className="w-[65px]" src="/logo.png" />
+            <p className="text-4xl text-white flex"> Nimory</p>
+          </div>
+        </Link>
         <div className="flex justify-center items-center gap-5 mr-20">
-          <button className="bg-red-600 rounded-md w-20 h-8" >
-            <p className="text-1xl text-white font-extrabold">Login</p>
+          <button
+            className="bg-red-600 rounded-md w-20 h-8"
+            onClick={() => {
+              setLogin(!login);
+            }}
+          >
+            <p className="text-1xl text-white font-bold">
+              {login ? "Sign in" : "Log in"}
+            </p>
           </button>
         </div>
       </div>
-
       <Backgroung />
     </div>
   );
